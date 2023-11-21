27914aecb448eeaeee6fcff5ea69f643b9c6a7dd
sujung-lim <sujunglim94@gmail.com>
2023-11-21 14:15:54 +0900

    빌드 후 변경 사항 커밋

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <GlobalStyles />
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </AppContainer>
